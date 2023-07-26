b46b68d975c092c802a5ce798bfddba044401e7c
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-07-26 15:32:27 +0200

    feat: better exception handling

--- src/zulip_write_only_proxy/repository.py
+++ src/zulip_write_only_proxy/repository.py
@@ -19,10 +19,11 @@ class JSONRepository(BaseModel):
     def put(self, client: ScopedClient) -> None:
         with self.path.open("rb") as f:
             data = orjson.loads(f.read())
-            if client.proposal_no in data.values():
-                raise ValueError("Client already exists")
+            proposal_nos = [value["proposal_no"] for value in data.values()]
+            if client.proposal_no in proposal_nos:
+                raise ValueError(f"Client already exists for {client.proposal_no=}")
 
-            data[client.key] = client.model_dump(exclude={"token"})
+            data[client.key] = client.model_dump(exclude={"key"})
 
         with self.path.open("wb") as f:
             f.write(orjson.dumps(data, option=orjson.OPT_INDENT_2))
