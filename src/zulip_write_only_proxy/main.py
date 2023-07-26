18720fbb1b51f18326adeaad27ac5ad84f2f89b2
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-07-26 11:29:23 +0200

    feat(endpoint): allow for image uploads

--- src/zulip_write_only_proxy/main.py
+++ src/zulip_write_only_proxy/main.py
@@ -1,4 +1,7 @@
 from contextlib import asynccontextmanager
+from io import FileIO
+from tempfile import SpooledTemporaryFile
+from typing import Annotated
 
 import fastapi
 import uvicorn
@@ -18,17 +21,29 @@ app = fastapi.FastAPI(title="Zulip Write Only Proxy", lifespan=lifespan)
 
 @app.post("/message")
 def post_message(
-    stream: str,
-    topic: str,
-    content: str,
+    stream: str = "DAMNIT!",
+    topic: str = "test-read-only-thing",
+    content: str = "test message",
     client: zulip.Client = fastapi.Depends(zulip_client.get_client),
+    image: fastapi.UploadFile = fastapi.File(None),
 ):
+    if image:
+        # Some screwing around to get the spooled tmp file to act more like a real file
+        # since Zulip needs it to have a filename
+
+        f: SpooledTemporaryFile = image.file  # type: ignore
+        f._file.name = image.filename  # type: ignore
+        result = client.upload_file(f)
+        print(result)
+        content += f" []({result['uri']})"
+
     request = {
         "type": "stream",
         "to": stream,
         "topic": topic,
         "content": content,
     }
+
     return client.send_message(request)
 
 
