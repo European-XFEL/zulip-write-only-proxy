a485143a9c0dc14f07778510b47669fea8be6324
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-07-26 12:26:34 +0200

    chore: add docstrings, add cli as script

--- src/zulip_write_only_proxy/cli.py
+++ src/zulip_write_only_proxy/cli.py
@@ -1,18 +1,21 @@
 import typer
+import uvicorn
 
-from . import service
+from . import service, main
 
 app = typer.Typer()
 
 
 @app.command()
 def create(proposal_no: int):
+    """Create a new entry for a proposal with the given number."""
     proposal = service.create_proposal(proposal_no)
     typer.echo(proposal)
 
 
 @app.command()
 def list():
+    """List all proposal entries."""
     proposals = service.list_proposals()
     typer.echo(proposals)
 
