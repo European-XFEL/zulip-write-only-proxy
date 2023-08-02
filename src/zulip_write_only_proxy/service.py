50a4ca9fb7897e28a3d888cf62021ea639f7d537
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-08-02 11:50:59 +0200

    style: more consistent imports

--- src/zulip_write_only_proxy/service.py
+++ src/zulip_write_only_proxy/service.py
@@ -6,38 +6,37 @@ import zulip
 from pydantic.fields import ModelPrivateAttr
 from pydantic_core import PydanticUndefined
 
-from .model import AdminClient, Client, ScopedClient
-from .repository import JSONRepository
+from . import model, repository
 
-REPOSITORY = JSONRepository(path=Path.cwd() / "clients.json")
+REPOSITORY = repository.JSONRepository(path=Path.cwd() / "clients.json")
 
 
-def create_client(proposal_no: int, stream: str | None = None) -> ScopedClient:
-    client = ScopedClient.create(proposal_no, stream)
+def create_client(proposal_no: int, stream: str | None = None) -> model.ScopedClient:
+    client = model.ScopedClient.create(proposal_no, stream)
     REPOSITORY.put(client)
     return client
 
 
-def create_admin() -> AdminClient:
-    client = AdminClient.create()
+def create_admin() -> model.AdminClient:
+    client = model.AdminClient.create()
     REPOSITORY.put_admin(client)
     return client
 
 
-def get_client(key: str) -> Client:
+def get_client(key: str) -> model.Client:
     return REPOSITORY.get(key)
 
 
-def list_clients() -> list[ScopedClient]:
+def list_clients() -> list[model.ScopedClient]:
     return REPOSITORY.list()
 
 
 def setup():
-    if not isinstance(ScopedClient._client, ModelPrivateAttr):
+    if not isinstance(model.ScopedClient._client, ModelPrivateAttr):
         raise RuntimeError("ScopedClient.client is not a ModelPrivateAttr")
 
-    client_default = ScopedClient._client.default
+    client_default = model.ScopedClient._client.default
 
     if client_default is None or client_default is PydanticUndefined:
         zulip_client = zulip.Client(config_file=str(Path.cwd() / "zuliprc"))
-        ScopedClient._client.default = zulip_client
+        model.ScopedClient._client.default = zulip_client
