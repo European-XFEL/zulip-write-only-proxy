868631f76512b9e08c24b545c8c695068ac62340
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-08-03 20:02:53 +0200

    refactor: import module not funcs, add annotations

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,44 +1,39 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from zulip_write_only_proxy.models import AdminClient, ScopedClient
-from zulip_write_only_proxy.services import (
-    create_admin,
-    create_client,
-    get_client,
-    list_clients,
-    setup,
-)
+from zulip_write_only_proxy.repositories import JSONRepository
+from zulip_write_only_proxy import services
 
 
-def test_create_client(repository):
-    result = create_client(proposal_no=3)
+def test_create_client(repository: JSONRepository):
+    result = services.create_client(proposal_no=3)
     assert isinstance(result, ScopedClient)
 
 
-def test_create_admin(repository):
-    result = create_admin()
+def test_create_admin(repository: JSONRepository):
+    result = services.create_admin()
     assert isinstance(result, AdminClient)
 
 
-def test_get_client(repository):
-    result = get_client("client1")
+def test_get_client(repository: JSONRepository):
+    result = services.get_client("client1")
 
     assert isinstance(result, ScopedClient)
     assert result.stream == "Test Stream 1"
     assert result.proposal_no == 1
 
     with pytest.raises(KeyError):
-        get_client("invalid")
+        services.get_client("invalid")
 
 
-def test_list_clients(repository):
-    result = list_clients()
+def test_list_clients(repository: JSONRepository):
+    result = services.list_clients()
     assert len(result) == 3
 
 
-def test_setup(zulip_client):
+def test_setup(zulip_client: MagicMock):
     from pydantic.fields import ModelPrivateAttr
     from pydantic_core import PydanticUndefinedType
 
@@ -47,14 +42,14 @@ def test_setup(zulip_client):
         assert isinstance(ScopedClient._client.default, PydanticUndefinedType)
         assert ScopedClient._client.default_factory is None
 
-        setup()
+        services.setup()
 
         assert not isinstance(
             ScopedClient._client.default_factory, PydanticUndefinedType
         )
 
 
-def test_setup_raises(zulip_client):
+def test_setup_raises(zulip_client: MagicMock):
     with patch.object(ScopedClient, "_client", 1234):
         with pytest.raises(RuntimeError):
-            setup()
+            services.setup()
