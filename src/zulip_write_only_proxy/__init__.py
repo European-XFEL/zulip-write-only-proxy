7cdd57b76bbea56c81ceb9f953b23868de836877
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-11-07 14:43:17 +0100

    style: format, fix mypy error

--- src/zulip_write_only_proxy/__init__.py
+++ src/zulip_write_only_proxy/__init__.py
@@ -2,6 +2,6 @@ try:
     from ._version import __version__, __version_tuple__
 except ImportError:
     __version__ = "unknown"
-    __version_tuple__ = (0, 0, 0)
+    __version_tuple__ = (0, 0, 0)  # type: ignore[assignment]
 
 __all__ = ["__version__", "__version_tuple__"]
