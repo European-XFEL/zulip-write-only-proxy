400153a1d799457664912550da847cd3b7100462
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-11-21 10:06:00 +0100

    feat(mymdc): improve docs, raise custom exception

--- src/zulip_write_only_proxy/mymdc.py
+++ src/zulip_write_only_proxy/mymdc.py
@@ -1,9 +1,15 @@
+"""Async MyMdC Client
+
+TODO: I've copy-pasted this code across a few different projects, when/if an async HTTPX
+MyMdC client package is created this can be removed and replaced with calls to that."""
 import datetime as dt
 from typing import Any, AsyncGenerator
 
 import httpx
 from pydantic import HttpUrl, SecretStr, ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from zulip_write_only_proxy.exceptions import ZwopException
 
 
 class MyMdCCredentials(BaseSettings):
@@ -27,6 +33,8 @@ class MyMdCAuth(httpx.Auth, MyMdCCredentials):
     async def acquire_token(self):
         """Acquires a new token if none is stored or if the existing token expired,
         otherwise reuses the existing token.
+
+        Token data stored under `_access_token` and `_expires_at`.
         """
         if self._access_token and self._expires_at >= dt.datetime.now():
             return self._access_token
@@ -59,7 +67,12 @@ class MyMdCAuth(httpx.Auth, MyMdCCredentials):
     async def async_auth_flow(
         self, request: httpx.Request
     ) -> AsyncGenerator[httpx.Request, Any]:
-        """Adds required authorization headers to the request."""
+        """Fetches bearer token (if required) and adds required authorization headers to
+        the request.
+
+        Yields:
+            AsyncGenerator[httpx.Request, Any]: yields `request` with additional headers
+        """
         bearer_token = await self.acquire_token()
 
         request.headers["Authorization"] = f"Bearer {bearer_token}"
@@ -69,27 +82,56 @@ class MyMdCAuth(httpx.Auth, MyMdCCredentials):
         yield request
 
 
+class NoStreamForProposalError(ZwopException):
+    """Raised when no stream name is found for a given proposal number, can occur if the
+    proposal does not have a Zulip eLog configured, or if the proposal does not exist.
+    """
+
+    def __init__(self, proposal_no: int):
+        super().__init__(
+            status_code=404, detail=f"No stream name found for proposal {proposal_no}"
+        )
+
+
 class MyMdCClient(httpx.AsyncClient):
     def __init__(self, auth: MyMdCAuth | None = None) -> None:
         """Client for the MyMdC API."""
         if auth is None:
-            auth = MyMdCAuth()  # type: ignore
+            auth = MyMdCAuth()  # type: ignore[call-arg]
 
         super().__init__(auth=auth)
 
-    async def get_zulip_stream_name(self, proposal_no: int) -> str | None:
-        """Get the Zulip stream name for a given proposal number."""
+    async def get_zulip_stream_name(self, proposal_no: int) -> str:
+        """Get the Zulip stream name for a given proposal number.
+
+        Raises:
+            NoStreamForProposalError: if no stream name is found for the proposal, or if
+            the proposal is non-existent.
+
+        Returns:
+            str: The stream name.
+        """
         # TODO: should use `/proposals/{number}/logbook`, but this responds with 403
         res = await self.get(
             f"https://in.xfel.eu/metadata/api/proposals/by_number/{proposal_no}"
         )
 
-        return res.json().get("logbook_info", {}).get("logbook_identifier", None)
+        res = res.json().get("logbook_info", {}).get("logbook_identifier", None)
+
+        if res is None:
+            raise NoStreamForProposalError(proposal_no)
+
+        if not isinstance(res, str):
+            raise RuntimeError(f"stream name should be string not {type(res)=} {res=}")
+
+        return res
 
 
+# crappy "dependency injection"/singleton. Other modules should not instantiate their
+# own client and should instead import this client instance.
 try:
     client = MyMdCClient()
     """Singleton instance of the MyMdC client."""
 except ValidationError as e:
     print(e)  # TODO: better logging
-    client = None
+    client = None  # type: ignore[assignment]
