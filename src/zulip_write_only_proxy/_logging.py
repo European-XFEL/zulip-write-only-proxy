1a8f6fca71141af6f2e468323ef8b37cd32a5282
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-13 13:39:44 +0100

    feat(logging): wrap all in config, add callsite parameters

--- src/zulip_write_only_proxy/_logging.py
+++ src/zulip_write_only_proxy/_logging.py
@@ -1,71 +1,70 @@
-import inspect
 import logging
 import sys
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, Sequence
 
+import colorama
 import structlog
+import structlog.typing
 from starlette.middleware.base import BaseHTTPMiddleware
 from structlog.stdlib import ProcessorFormatter
-from structlog.types import Processor
-
-from .settings import settings
 
 if TYPE_CHECKING:
     from starlette.requests import Request
     from starlette.responses import Response
 
 
-def get_logger():
-    frame = inspect.stack()[1]
-    module = inspect.getmodule(frame[0])
-    module_name = module.__name__ if module else __package__
-    return structlog.get_logger(logger_name=module_name.replace(f"{__package__}.", "."))
+def parameter_parser(logger, method_name, event_dict):
+    event_dict["logger_name"] = ".".join(
+        [event_dict.pop("module", None), event_dict.pop("func_name", None)]
+    )
+    return event_dict
 
 
-if settings.debug:
-    level = logging.DEBUG
-    renderer = structlog.dev.ConsoleRenderer()
-    # sentry_processor = sentry.SentryProcessor(level=logging.INFO)
-else:
-    level = logging.INFO
-    renderer = structlog.processors.JSONRenderer(indent=1)
-    # sentry_processor = sentry.SentryJsonProcessor(level=logging.WARNING)
-
-shared_processors = [
-    structlog.contextvars.merge_contextvars,
-    structlog.processors.add_log_level,
-    structlog.processors.StackInfoRenderer(),
-    structlog.dev.set_exc_info,
-    # structlog.processors.CallsiteParameterAdder(
-    #     {
-    #         structlog.processors.CallsiteParameter.FILENAME,
-    #         structlog.processors.CallsiteParameter.LINENO,
-    #     }
-    # ),
-    structlog.processors.TimeStamper(fmt="%Y-%m-%dT%H:%M:%SZ", utc=True),
-]
-
-structlog_processors = shared_processors + [renderer]
-logging_processors: List[Processor] = [
-    ProcessorFormatter.remove_processors_meta,
-    renderer,
-]
-
-# if sentry.SENTRY_ENABLED:
-#     processors.append(sentry_processor)
-
-formatter = ProcessorFormatter(
-    # These run ONLY on `logging` entries that do NOT originate within structlog
-    foreign_pre_chain=shared_processors,
-    # These run on ALL entries after the pre_chain is done
-    processors=logging_processors,
-)
-
-
-def configure() -> None:
+def configure(debug: bool) -> None:
     """
     Configures logging and sets up Uvicorn to use Structlog.
     """
+
+    level = logging.DEBUG if debug else logging.INFO
+    level_styles = structlog.dev.ConsoleRenderer.get_default_level_styles()
+
+    if debug:
+        level_styles["debug"] = colorama.Fore.MAGENTA
+
+    renderer: structlog.typing.Processor = (
+        structlog.dev.ConsoleRenderer(colors=True, level_styles=level_styles)  # type: ignore[assignment]
+        if debug
+        else structlog.processors.JSONRenderer(indent=1)
+    )
+
+    # sentry_processor = sentry.SentryProcessor(level=level)
+
+    shared_processors: Sequence[structlog.typing.Processor] = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        structlog.dev.set_exc_info,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%dT%H:%M:%SZ", utc=True),
+        structlog.processors.CallsiteParameterAdder(
+            {
+                structlog.processors.CallsiteParameter.MODULE,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+            }
+        ),
+        parameter_parser,
+    ]
+
+    structlog_processors = [*shared_processors, renderer]
+    logging_processors = [ProcessorFormatter.remove_processors_meta, renderer]
+
+    # if sentry.SENTRY_ENABLED:
+    #     processors.append(sentry_processor)
+
+    formatter = ProcessorFormatter(
+        foreign_pre_chain=shared_processors,
+        processors=logging_processors,
+    )
+
     structlog.configure(
         processors=structlog_processors,
         wrapper_class=structlog.make_filtering_bound_logger(level),
@@ -74,18 +73,17 @@ def configure() -> None:
         context_class=dict,
     )
 
-    log = structlog.get_logger(logger_name=__name__.replace("kourou.", "."))
-
     handler = logging.StreamHandler(sys.stderr)
     handler.setFormatter(formatter)
     logging.basicConfig(handlers=[handler], level=logging.INFO)
 
-    configure_uvicorn()
+    configure_uvicorn(renderer, shared_processors)
 
+    log = structlog.get_logger()
     log.info("Configured Logging")
 
 
-def configure_uvicorn():
+def configure_uvicorn(renderer, shared_processors):
     import uvicorn.config
 
     uvicorn.config.LOGGING_CONFIG["formatters"]["default"] = {
@@ -109,8 +107,7 @@ def configure_uvicorn():
 
 
 class RequestLoggingMiddleware(BaseHTTPMiddleware):
-
-    log = structlog.get_logger(logger_name="uvicorn.access")
+    log = structlog.get_logger()
 
     async def dispatch(self, request: "Request", call_next) -> "Response":
         """Add a middleware to FastAPI that will log requests and responses,
@@ -123,10 +120,10 @@ class RequestLoggingMiddleware(BaseHTTPMiddleware):
         }
 
         if request.query_params:
-            info["query_params"] = request.query_params
+            info["query_params"] = str(request.query_params)
 
         if request.path_params:
-            info["path_params"] = request.path_params
+            info["path_params"] = str(request.path_params)
 
         self.log.debug("Request", **info)
         response = await call_next(request)
