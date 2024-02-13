110e710205d6a35142ed0ed508b9c1646ee997d6
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-13 13:38:21 +0100

    feat(repositories): use models as config, remove admin client

--- src/zulip_write_only_proxy/repositories.py
+++ src/zulip_write_only_proxy/repositories.py
@@ -1,9 +1,8 @@
 import threading
-from pathlib import Path
 
 import orjson
 import zulip
-from pydantic import BaseModel, SecretStr, field_validator, validate_call
+from pydantic import BaseModel, DirectoryPath, FilePath, SecretStr, validate_call
 
 from . import models
 
@@ -11,7 +10,7 @@ file_lock = threading.Lock()
 
 
 class ZuliprcRepository(BaseModel):
-    directory: Path
+    directory: DirectoryPath
 
     def get(self, key: str) -> zulip.Client:
         return zulip.Client(config_file=str(self.directory / f"{key}.zuliprc"))
@@ -28,44 +27,29 @@ site={site}
         return zulip.Client(config_file=str(self.directory / f"{name}.zuliprc"))
 
     def list(self):
-        return [
-            self.get(p.stem) for p in self.directory.iterdir() if p.suffix == ".zuliprc"
-        ]
+        return [p.stem for p in self.directory.iterdir() if p.suffix == ".zuliprc"]
 
 
 class ClientRepository(BaseModel):
     """A basic file/JSON-based repository for storing client entries."""
 
-    path: Path
+    path: FilePath
 
-    def get(self, key: str) -> models.Client:
+    def get(self, key: str) -> models.ScopedClient:
         data = orjson.loads(self.path.read_bytes())
         client_data = data[key]
 
-        if client_data.get("admin"):
-            return models.AdminClient(key=SecretStr(key), **client_data)
-
         return models.ScopedClient(key=SecretStr(key), **client_data)
 
-    def put(self, client: models.Client) -> None:
+    def put(self, client: models.ScopedClient) -> None:
         with file_lock:
             data: dict[str, dict] = orjson.loads(self.path.read_bytes())
             data[client.key.get_secret_value()] = client.model_dump(exclude={"key"})
             self.path.write_bytes(orjson.dumps(data, option=orjson.OPT_INDENT_2))
 
-    def list(self) -> list[models.ScopedClient]:
+    def list(self) -> list[models.ScopedClientWithKey]:
         data = orjson.loads(self.path.read_bytes())
 
         return [
-            models.ScopedClient(key=key, **value)
-            for key, value in data.items()
-            if not value.get("admin")
+            models.ScopedClientWithKey(key=key, **value) for key, value in data.items()
         ]
-
-    @field_validator("path")
-    @classmethod
-    def check_path(cls, v: Path) -> Path:
-        if not v.exists():
-            v.touch()
-            v.write_text("{}")
-        return v
