f432cbf1c7e0a9552f618801d1ca52490787c7b6
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-13 13:42:15 +0100

    feat: update entrypoint after major changes

--- src/zulip_write_only_proxy/__init__.py
+++ src/zulip_write_only_proxy/__init__.py
@@ -4,9 +4,13 @@ except ImportError:
     __version__ = "unknown"
     __version_tuple__ = (0, 0, 0)  # type: ignore[assignment]
 
+from structlog import get_logger
+
 from . import _logging
-from ._logging import get_logger
+from .settings import settings
 
-_logging.configure()
+_logging.configure(settings.debug)
 
-__all__ = ["__version__", "__version_tuple__", "get_logger"]
+logger = get_logger()
+
+__all__ = ["__version__", "__version_tuple__", "logger"]
