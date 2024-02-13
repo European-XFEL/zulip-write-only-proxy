c1abed84e42e2874583091b8edf839c2cb9a4f2f
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-13 10:32:06 +0100

    feat(logging): FIXUP

--- src/zulip_write_only_proxy/__init__.py
+++ src/zulip_write_only_proxy/__init__.py
@@ -4,4 +4,9 @@ except ImportError:
     __version__ = "unknown"
     __version_tuple__ = (0, 0, 0)  # type: ignore[assignment]
 
-__all__ = ["__version__", "__version_tuple__"]
+from . import _logging
+from ._logging import get_logger
+
+_logging.configure()
+
+__all__ = ["__version__", "__version_tuple__", "get_logger"]
