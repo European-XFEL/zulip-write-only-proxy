a3fcb18e187875dccfd3c6164e7dede94fa820da
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-13 13:42:15 +0100

    feat: update entrypoint after major changes

--- src/zulip_write_only_proxy/main.py
+++ src/zulip_write_only_proxy/main.py
@@ -1,147 +1,69 @@
 from __future__ import annotations
 
-import logging
 from contextlib import asynccontextmanager
-from tempfile import SpooledTemporaryFile
-from typing import Annotated, Union
 
 import fastapi
-from fastapi.security import APIKeyHeader
+from starlette.middleware.sessions import SessionMiddleware
 
-from . import __version__, __version_tuple__, _logging, models, services
+from . import logger, routers, services
+from ._logging import RequestLoggingMiddleware
+from .settings import settings
 
 
 @asynccontextmanager
 async def lifespan(app: fastapi.FastAPI):
-    logging.getLogger("uvicorn.access").addFilter(_logging.EndpointFilter())
-    services.setup()
+    from . import mymdc
+
+    services.configure(settings, app)
+
+    mymdc.configure(settings, app)
+
+    for module in [routers.api, routers.auth, routers.frontend]:
+        app.include_router(module.router)
+
+        if hasattr(module, "configure"):
+            module.configure(settings, app)
+
     yield
 
 
-app = fastapi.FastAPI(title="Zulip Write Only Proxy", lifespan=lifespan)
-
-api_key_header = APIKeyHeader(name="X-API-key")
-
-
-def get_client(key: Annotated[str, fastapi.Security(api_key_header)]) -> models.Client:
-    try:
-        return services.get_client(key)
-    except KeyError as e:
-        raise fastapi.HTTPException(status_code=401, detail="Unauthorised") from e
-
-
-_docs_url = "https://zulip.com/api/send-message#response"
-
-
-@app.post(
-    "/send_message",
-    tags=["User"],
-    response_description=f"See <a href='{_docs_url}'>{_docs_url}</a>",
+app = fastapi.FastAPI(
+    title="Zulip Write Only Proxy",
+    lifespan=lifespan,
+    exception_handlers={
+        routers.frontend.AuthException: routers.frontend.auth_redirect,
+    },
 )
-def send_message(
-    client: Annotated[models.ScopedClient, fastapi.Depends(get_client)],
-    topic: Annotated[str, fastapi.Query(...)],
-    content: Annotated[str, fastapi.Body(...)],
-    image: Annotated[Union[fastapi.UploadFile, None], fastapi.File()] = None,
-):
-    if image:
-        # Some screwing around to get the spooled tmp file to act more like a real file
-        # since Zulip needs it to have a filename
-        f: SpooledTemporaryFile = image.file  # type: ignore[assignment]
-        f._file.name = image.filename  # type: ignore[misc, assignment]
-
-        result = client.upload_file(f)
-
-        content += f"\n\n[]({result['uri']})"
-
-    return client.send_message(topic, content)
 
 
-_docs_url = "https://zulip.com/api/update-message#response"
-
-
-@app.patch(
-    "/update_message",
-    tags=["User"],
-    response_description=f"See <a href='{_docs_url}'>{_docs_url}</a>",
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=settings.session_secret.get_secret_value(),
 )
-def update_message(
-    client: Annotated[models.ScopedClient, fastapi.Depends(get_client)],
-    message_id: Annotated[int, fastapi.Query(...)],
-    propagate_mode: Annotated[models.PropagateMode, fastapi.Query(...)],
-    content: Annotated[str | None, fastapi.Body(media_type="text/plain")] = None,
-    topic: Annotated[str | None, fastapi.Query()] = None,
-):
-    if content or topic:
-        return client.update_message(topic, content, message_id, propagate_mode)
-    else:
-        raise fastapi.HTTPException(
-            status_code=400,
-            detail=(
-                "Either content (update message text) or topic (rename message topic) "
-                "must be provided"
-            ),
-        )
 
+app.add_middleware(RequestLoggingMiddleware)
 
-_docs_url = "https://zulip.com/api/upload-file#response"
+if __name__ == "__main__":
+    import uvicorn
 
+    logger.info(
+        "Starting uvicorn",
+        app=f"{__package__}.main:app",
+        port=settings.address.port or 8000,
+        log_level=settings.log_level,
+        host=settings.address.host or "127.0.0.1",
+        proxy_headers=True,
+        reload=False,
+        root_path=settings.proxy_root_path,
+    )
 
-@app.post(
-    "/upload_file",
-    tags=["User"],
-    response_description=f"See <a href='{_docs_url}'>{_docs_url}</a>",
-)
-def upload_file(
-    client: Annotated[models.ScopedClient, fastapi.Depends(get_client)],
-    file: Annotated[fastapi.UploadFile, fastapi.File(...)],
-):
-    f: SpooledTemporaryFile = file.file  # type: ignore[assignment]
-    f._file.name = file.filename  # type: ignore[misc, assignment]
-
-    return client.upload_file(f)
-
-
-_docs_url = "https://zulip.com/api/get-stream-topics#response"
-
-
-@app.get(
-    "/get_stream_topics",
-    tags=["User"],
-    response_description=f"See <a href='{_docs_url}'>{_docs_url}</a>",
-)
-def get_stream_topics(
-    client: Annotated[models.ScopedClient, fastapi.Depends(get_client)],
-):
-    return client.get_stream_topics()
-
-
-@app.get("/me", tags=["User"], response_model_exclude={"key"})
-def get_me(
-    client: Annotated[models.Client, fastapi.Depends(get_client)]
-) -> models.Client:
-    return client
-
-
-@app.get("/health", tags=["Admin"])
-def healthcheck():
-    return {
-        "status": "OK",
-        "dirty": "dirty" in __version__,
-        "dev": "+" in __version__,
-        "version": __version__,
-        "version_tuple": __version_tuple__,
-    }
-
-
-@app.post("/create_client", tags=["Admin"])
-async def create_client(
-    admin_client: Annotated[models.AdminClient, fastapi.Depends(get_client)],
-    client: Annotated[models.ScopedClient, fastapi.Depends(services.create_client)],
-) -> models.ScopedClientWithKey:
-    try:
-        dump = client.model_dump()
-        dump["key"] = client.key.get_secret_value()
-        return models.ScopedClientWithKey(**dump)
-    except ValueError as e:
-        raise fastapi.HTTPException(status_code=400, detail=str(e)) from e
+    uvicorn.run(
+        app=f"{__package__}.main:app",
+        port=settings.address.port or 8000,
+        log_level=settings.log_level,
+        host=settings.address.host or "127.0.0.1",
+        proxy_headers=True,
+        # reload=settings.debug,
+        reload=False,
+        root_path=settings.proxy_root_path,
+    )
