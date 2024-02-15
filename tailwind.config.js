3dddea40cecfdb197e8e72bd0301976e0d40350f
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-15 15:21:06 +0100

    chore: tidy files, minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/zulip_write_only_proxy/templates/**.html",
-    "./src/zulip_write_only_proxy/templates/*/**.html"
+    "./src/zulip_write_only_proxy/frontend/templates/**.html",
+    "./src/zulip_write_only_proxy/frontend/templates/*/**.html"
   ],
   theme: {
     extend: {},
