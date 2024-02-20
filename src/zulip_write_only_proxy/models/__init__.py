c1628e8e37dc405d4801bf44827af1fefa2c02d6
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-02-20 09:13:56 +0100

    refactor: split models out into module

---
+++ src/zulip_write_only_proxy/models/__init__.py
@@ -0,0 +1,12 @@
+from .client import ScopedClient, ScopedClientCreate, ScopedClientWithKey
+from .zulip import BotConfig, Message, Messages, PropagateMode
+
+__all__ = [
+    "ScopedClient",
+    "ScopedClientCreate",
+    "ScopedClientWithKey",
+    "BotConfig",
+    "Message",
+    "Messages",
+    "PropagateMode",
+]
