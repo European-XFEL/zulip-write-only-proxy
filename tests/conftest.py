0269e85ac0e4cbc85a82dd88120b5b73e460b272
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-03-12 16:16:08 +0100

    test(conftest): update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,65 +1,33 @@
-import tempfile
-from pathlib import Path
+from datetime import datetime
 from unittest.mock import AsyncMock, MagicMock, patch
 
-import orjson
 import pytest
+import pytest_asyncio
 from fastapi.testclient import TestClient
+from pydantic import SecretStr
+from pydantic_core import Url
 
-from zulip_write_only_proxy.models import ScopedClient
-from zulip_write_only_proxy.repositories import ClientRepository, ZuliprcRepository
-
-
-@pytest.fixture(autouse=True)
-def client_repo():
-    with tempfile.NamedTemporaryFile(delete=False) as f:
-        data = {
-            "client1": {
-                "stream": "Test Stream 1",
-                "proposal_no": 1,
-                "bot_name": "1",
-            },
-            "client2": {
-                "stream": "Test Stream 2",
-                "proposal_no": 2,
-                "bot_name": "2",
-            },
-            "admin1": {
-                "admin": True,
-            },
-        }
-
-        f.write(orjson.dumps(data))
-
-        path = Path(f.name)
-
-    client_repo = ClientRepository(path=path)
-
-    with patch("zulip_write_only_proxy.services.CLIENT_REPO", client_repo):
-        yield client_repo
-
-    path.unlink()
+from zulip_write_only_proxy.models import BotConfig, ScopedClient
+from zulip_write_only_proxy.settings import settings as base_settings
 
 
 @pytest.fixture(autouse=True)
-def zuliprc_repo(tmp_path_factory):
-    directory = tmp_path_factory.mktemp("config")
-    zuliprc = directory / "bot.zuliprc"
-    zuliprc.write_text("[api]\nemail=email\nkey=key\nsite=site\n")
+def settings(tmp_path_factory):
+    """Configure settings for tests.
 
-    zuliprc_repo = ZuliprcRepository(directory=directory)
+    - Set the config_dir to a temporary directory
+    - Remove  credentials
+    """
+    config_dir = tmp_path_factory.mktemp("config")
+    base_settings.config_dir = config_dir
 
-    with patch("zulip_write_only_proxy.services.ZULIPRC_REPO", zuliprc_repo):
-        yield zuliprc_repo
+    base_settings.auth.client_id = "client_id"
+    base_settings.auth.client_secret = SecretStr("client_secret")
 
-    zuliprc.unlink()
+    base_settings.mymdc.secret = SecretStr("mymdc_secret")
+    base_settings.mymdc.token_url = Url("http://foobar.local/token")
 
-
-@pytest.fixture()
-def scoped_client():
-    client = ScopedClient(proposal_no=1234, stream="Test Stream", bot_name="Test Bot")
-    client._client = MagicMock()
-    return client
+    return base_settings
 
 
 @pytest.fixture(autouse=True)
@@ -69,10 +37,48 @@ def zulip_client():
         yield mock_class
 
 
+@pytest.fixture()
+def a_scoped_client(zulip_client):
+    client = ScopedClient(
+        proposal_no=1234,
+        stream="Test Stream",
+        bot_id=1,
+        bot_site=Url("http://a-site.com"),
+        token=SecretStr("secret"),
+        created_by="foo",
+        created_at=datetime.fromisoformat("2021-01-01Z00:00:00"),
+    )
+    client._client = zulip_client
+    return client
+
+
+@pytest.fixture()
+def a_zuliprc():
+    return BotConfig(
+        email="foo@bar.com",
+        key=SecretStr("secret"),
+        site=Url("http://a-site.com"),
+        id=1,
+        proposal_no=1234,
+        created_at=datetime.fromisoformat("2021-01-01Z00:00:00"),
+    )
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def _services(settings, a_zuliprc, a_scoped_client):
+    from zulip_write_only_proxy import services
+
+    await services.configure(settings, None)
+
+    await services.CLIENT_REPO.insert(a_scoped_client)
+
+    await services.ZULIPRC_REPO.insert(a_zuliprc)
+
+
 @pytest.fixture(autouse=True)
 def mymdc_client():
     with patch(
-        "zulip_write_only_proxy.mymdc.client", new_callable=AsyncMock
+        "zulip_write_only_proxy.mymdc.CLIENT", new_callable=AsyncMock
     ) as mock_class:
         mock_class.return_value = mock_class
         mock_class.get_zulip_bot_credentials.return_value = ("key", "email")
@@ -83,4 +89,6 @@ def mymdc_client():
 def fastapi_client(client_repo, zulip_client):
     from zulip_write_only_proxy import main
 
-    return TestClient(main.app, headers={"X-API-key": "client1"})
+    app = main.create_app()
+
+    return TestClient(app, headers={"X-API-key": "client1"})
