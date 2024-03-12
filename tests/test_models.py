d01f142b826aef74dac8d680eefe56ece201dd55
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-03-12 16:16:21 +0100

    test(models): update model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,64 +1,64 @@
 import io
 from unittest.mock import MagicMock
 
-import pytest
-
-from zulip_write_only_proxy.models import AdminClient
+from structlog.testing import capture_logs
 
 
-def test_upload_file(scoped_client):
-    scoped_client._client.upload_file = MagicMock(return_value={"uri": "/foo/bar.jpg"})
+def test_upload_file(a_scoped_client):
+    a_scoped_client._client.upload_file = MagicMock(
+        return_value={"uri": "/foo/bar.jpg"}
+    )
 
     file = io.BytesIO(b"test file data")
 
-    result = scoped_client.upload_file(file)
+    result = a_scoped_client.upload_file(file)
 
-    scoped_client._client.upload_file.assert_called_once_with(file)
+    a_scoped_client._client.upload_file.assert_called_once_with(file)
 
     assert result == {"uri": "/foo/bar.jpg"}
 
 
-def test_get_stream_topics(scoped_client):
-    scoped_client._client.get_stream_id = MagicMock(
+def test_get_stream_topics(a_scoped_client):
+    a_scoped_client._client.get_stream_id = MagicMock(
         return_value={"result": "success", "stream_id": 123}
     )
-    scoped_client._client.get_stream_topics = MagicMock(
+    a_scoped_client._client.get_stream_topics = MagicMock(
         return_value=["Topic 1", "Topic 2"]
     )
 
-    result = scoped_client.get_stream_topics()
+    result = a_scoped_client.get_stream_topics()
 
-    scoped_client._client.get_stream_id.assert_called_once_with("Test Stream")
+    a_scoped_client._client.get_stream_id.assert_called_once_with("Test Stream")
 
-    scoped_client._client.get_stream_topics.assert_called_once_with(123)
+    a_scoped_client._client.get_stream_topics.assert_called_once_with(123)
 
     assert result == ["Topic 1", "Topic 2"]
 
 
-def test_get_stream_topics_log(scoped_client, caplog: pytest.LogCaptureFixture):
-    scoped_client._client.get_stream_id = MagicMock(
-        return_value={
-            "code": "BAD_REQUEST",
-            "msg": "Invalid stream name 'nonexistent'",
-            "result": "error",
-        }
-    )
+def test_get_stream_topics_log(a_scoped_client):
+    response = {
+        "code": "BAD_REQUEST",
+        "msg": "Invalid stream name 'nonexistent'",
+        "result": "error",
+    }
 
-    scoped_client.get_stream_topics()
+    a_scoped_client._client.get_stream_id = MagicMock(return_value=response)
 
-    assert len(caplog.records) == 1
-    assert caplog.records[0].message == (
-        "failed to get stream id for Test Stream, zulip api response: "
-        f"{scoped_client._client.get_stream_id.return_value}"
-    )
+    with capture_logs() as caplog:
+        a_scoped_client.get_stream_topics()
+
+    assert len(caplog) == 1
+    assert caplog[0]["stream"] == "Test Stream"
+    assert caplog[0]["response"] == response
+    assert caplog[0]["event"] == "Failed to get stream id"
 
 
-def test_send_message(scoped_client):
-    scoped_client._client.send_message = MagicMock(return_value={"result": "success"})
+def test_send_message(a_scoped_client):
+    a_scoped_client._client.send_message = MagicMock(return_value={"result": "success"})
 
-    result = scoped_client.send_message("Test Topic", "Test Content")
+    result = a_scoped_client.send_message("Test Topic", "Test Content")
 
-    scoped_client._client.send_message.assert_called_once_with({
+    a_scoped_client._client.send_message.assert_called_once_with({
         "type": "stream",
         "to": "Test Stream",
         "topic": "Test Topic",
@@ -66,20 +66,3 @@ def test_send_message(scoped_client):
     })
 
     assert result == {"result": "success"}
-
-
-def test_admin_client_create():
-    client = AdminClient(admin=True)
-
-    assert client.token is not None
-
-    assert client.admin is True
-
-
-def test_admin_client_init():
-    result = AdminClient(key="", admin=True)  # type: ignore[assignment]
-
-    assert result.admin
-
-    with pytest.raises(ValueError):
-        AdminClient(key="", admin=False)  # type: ignore[assignment]
