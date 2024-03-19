7caf93185ac7408695cfdfc3f13ba4a3374cb7c4
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-03-19 08:51:03 +0100

    feat(api): split out mymdc endpoints, refactor

--- src/zulip_write_only_proxy/routers/__init__.py
+++ src/zulip_write_only_proxy/routers/__init__.py
@@ -1,3 +1,3 @@
-from . import api, auth, frontend
+from . import api, auth, frontend, mymdc
 
-__all__ = ["api", "auth", "frontend"]
+__all__ = ["api", "auth", "frontend", "mymdc"]
---
+++ src/zulip_write_only_proxy/routers/mymdc.py
@@ -0,0 +1,73 @@
+from typing import Annotated, TypeAlias
+
+import orjson
+from fastapi import APIRouter, Depends, HTTPException, Request, Response
+
+from .. import models, mymdc
+from .api import get_client
+
+ScopedClient: TypeAlias = Annotated[models.ScopedClient, Depends(get_client)]
+
+
+async def proxy_request(request: Request) -> Response:
+    path = request.scope["path"].replace("/api/mymdc", "/api")  # rewrite path for mymdc
+
+    res = await mymdc.CLIENT.get(path)
+
+    return Response(
+        content=res.content,
+        media_type=res.headers["Content-Type"],
+        headers=res.headers,
+        status_code=res.status_code,
+    )
+
+
+async def check_and_proxy_request(client: ScopedClient, request: Request) -> Response:
+    req_proposal_no = request.path_params.get("proposal_no")
+    if req_proposal_no and int(req_proposal_no) != client.proposal_no:
+        raise HTTPException(
+            status_code=403, detail="Client not scoped to this proposal"
+        )
+
+    res = await proxy_request(request)
+    content = orjson.loads(res.body)
+
+    res_proposal_id = content.get("proposal_id")
+    res_proposal_no = content.get("proposal_no")
+
+    if all(n is None for n in (req_proposal_no, res_proposal_id, res_proposal_no)):
+        raise HTTPException(
+            status_code=400, detail="Cannot determine proposal for response"
+        )
+
+    if any((
+        res_proposal_id == client.proposal_id,
+        res_proposal_no == client.proposal_no,
+    )):
+        return res
+
+    raise HTTPException(status_code=403, detail="Client not scoped to this proposal")
+
+
+router = APIRouter(prefix="/api/mymdc", tags=["mymdc"])
+
+
+@router.get("/proposals/by_number/{proposal_no}/runs")
+@router.get("/proposals/by_number/{proposal_no}")
+async def get_proposals_by_number(
+    proposal_no: int, res=Depends(check_and_proxy_request)
+):
+    return res
+
+
+@router.get("/proposals/by_number/{proposal_no}/runs/{run_number}")
+async def get_proposals_runs(
+    proposal_no: int, run_number: int, res=Depends(check_and_proxy_request)
+):
+    return res
+
+
+@router.get("/samples/{id}")
+@router.get("/experiments/{id}")
+async def get(id: int, res=Depends(check_and_proxy_request)):
+    return res
