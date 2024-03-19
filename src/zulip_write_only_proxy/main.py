2def2a5df1d4df676ba03b72e26a2fe58ad44790
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-03-19 08:52:38 +0100

    revert: refactor mymdc api router

--- src/zulip_write_only_proxy/main.py
+++ src/zulip_write_only_proxy/main.py
@@ -19,7 +19,7 @@ def create_app():
 
         mymdc.configure(settings, app)
 
-        for module in [routers.api, routers.auth, routers.frontend]:
+        for module in [routers.api, routers.auth, routers.frontend, routers.mymdc]:
             app.include_router(module.router)
 
             if hasattr(module, "configure"):
