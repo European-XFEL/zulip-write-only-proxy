85b6bfa13dc3544dbb48b89e55121a0cdf2e4982
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-09-24 10:59:32 +0200

    test(repositories): update for new abstract repo classes

--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -1,55 +1,67 @@
 import tempfile
+from datetime import datetime
 from pathlib import Path
 
 import pytest
-from pydantic import ValidationError
+from pydantic import SecretStr
+from pydantic_core import Url
 
-from zulip_write_only_proxy.models import AdminClient, ScopedClient
-from zulip_write_only_proxy.repositories import ClientRepository
+from zulip_write_only_proxy.models import ScopedClient
+from zulip_write_only_proxy.repositories import BaseRepository
 
 
-def test_file_creation():
+@pytest.mark.asyncio()
+async def test_file_creation():
     with tempfile.TemporaryDirectory() as f:
         path = Path(f) / "test.json"
 
         assert not path.exists()
 
-        repository = ClientRepository(path=path)
+        repo = BaseRepository(file=path, model=ScopedClient)
+
+        await repo.write()
 
         assert path.exists()
 
-        assert len(repository.list()) == 0
 
-
-def test_get_scoped_client(client_repo: ClientRepository):
-    result = client_repo.get("client1")
+@pytest.mark.asyncio()
+async def test_get_scoped_client(client_repo, a_scoped_client):
+    result = await client_repo.get(a_scoped_client._key)
 
     assert isinstance(result, ScopedClient)
-    assert result.stream == "Test Stream 1"
-    assert result.proposal_no == 1
 
-    with pytest.raises(KeyError):
-        client_repo.get("invalid")
+    assert result.model_dump() == a_scoped_client.model_dump()
 
-
-def test_get_admin_client(client_repo: ClientRepository):
-    result = client_repo.get("admin1")
-
-    assert isinstance(result, AdminClient)
-    assert result.admin is True
+    result = await client_repo.get("invalid")
+    assert result is None
 
 
-def test_put_scoped_client(client_repo: ClientRepository):
+@pytest.mark.asyncio()
+async def test_list_clients(client_repo, a_scoped_client):
+    result = await client_repo.list()
+
+    assert len(result) == 1
+
+    assert isinstance(result[0], ScopedClient)
+    assert result[0].model_dump() == a_scoped_client.model_dump()
+
+
+@pytest.mark.asyncio()
+async def test_insert_delete_scoped_client(client_repo):
     client = ScopedClient(
-        key="client3",  # type: ignore[arg-type]
-        stream="Test Stream 3",
-        proposal_no=3,
-        bot_name="Test Bot 3",
+        proposal_no=11234,
+        proposal_id=15678,
+        stream="Another Test Stream",
+        bot_id=1,
+        bot_site=Url("http://a-site.com"),
+        token=SecretStr("secret"),
+        created_by="foo",
+        created_at=datetime.fromisoformat("2021-01-01Z00:00:00"),
     )
 
-    client_repo.put(client)
+    await client_repo.insert(client)
 
-    result = client_repo.get(client.token.get_secret_value())
+    result = await client_repo.get(client._key)
 
     assert isinstance(result, ScopedClient)
     assert result.model_dump() == client.model_dump()
