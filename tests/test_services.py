391834e5599106f31b8257314619db7f5e137443
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-09-24 11:00:52 +0200

    test(services): simplify, fix after changes

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,48 +1,64 @@
 from __future__ import annotations
 
-import typing
+from datetime import datetime
+from unittest.mock import AsyncMock, patch
 
 import pytest
+from fastapi import HTTPException
+from pydantic import SecretStr
 
 from zulip_write_only_proxy import services
-from zulip_write_only_proxy.models import AdminClient, ScopedClient, ScopedClientCreate
-
-if typing.TYPE_CHECKING:
-    from zulip_write_only_proxy.repositories import ClientRepository
+from zulip_write_only_proxy.models import ScopedClient, ScopedClientCreate
+from zulip_write_only_proxy.mymdc import MyMdCResponseError
 
 
 @pytest.mark.asyncio()
-async def test_create_client(client_repo: ClientRepository):
-    result = await services.create_client(
-        ScopedClientCreate(proposal_no=1234, stream="Test Stream", bot_name="Test Bot"),
+async def test_create_client():
+    client = ScopedClientCreate(
+        proposal_no=11234,
+        stream="Another Test Stream",
+        bot_id=2,
+        bot_site="http://a-site.com",
+        token=SecretStr("another-secret"),
+        created_at=datetime.fromisoformat("2021-01-01Z00:00:00"),
     )
+
+    result = await services.create_client(client, created_by="foo")
     assert isinstance(result, ScopedClient)
 
 
 @pytest.mark.asyncio()
-async def test_create_client_no_bot(client_repo: ClientRepository):
-    result = await services.create_client(
-        ScopedClientCreate(proposal_no=1234, stream="Test Stream", bot_name="Test Bot"),
+async def test_create_client_no_bot():
+    import httpx
+
+    client = ScopedClientCreate(
+        proposal_no=111234,
+        stream="Another Test Stream",
+        bot_id=None,
+        token=SecretStr("another-secret"),
+        created_at=datetime.fromisoformat("2021-01-01Z00:00:00"),
     )
+
+    with patch(
+        "zulip_write_only_proxy.mymdc.CLIENT", new_callable=AsyncMock
+    ) as mock_class:
+        mock_class.return_value = mock_class
+        mock_class.get_zulip_bot_credentials.side_effect = MyMdCResponseError(
+            httpx.Response(status_code=403, json="{}")
+        )
+
+        result = await services.create_client(client, created_by="bar")
+
     assert isinstance(result, ScopedClient)
+    assert result.bot_id is None
 
 
-def test_create_admin(client_repo: ClientRepository):
-    result = services.create_admin()
-    assert isinstance(result, AdminClient)
-
-
-def test_get_client(client_repo: ClientRepository):
-    result = services.get_client("client1")
+@pytest.mark.asyncio()
+async def test_get_client(a_scoped_client):
+    result = await services.get_client(a_scoped_client.token.get_secret_value())
 
     assert isinstance(result, ScopedClient)
-    assert result.stream == "Test Stream 1"
-    assert result.proposal_no == 1
+    assert a_scoped_client.model_dump_json() == result.model_dump_json()
 
-    with pytest.raises(KeyError):
-        services.get_client("invalid")
-
-
-def test_list_clients(client_repo: ClientRepository):
-    result = services.list_clients()
-    assert len(result) == 2
+    with pytest.raises(HTTPException):
+        await services.get_client("invalid")
